15d6fa75078456a862b751de21206947277a95b1
Nanami <i@shiraha.cn>
2023-05-16 14:57:31 +0800

    docs: update CHANGELOG. 调整了代码块的样式

--- scripts/events/index.js
+++ scripts/events/index.js
@@ -14,5 +14,24 @@ hexo.on('generateBefore', () => {
 
 hexo.on('ready', () => {
   const { version } = require('../../package.json');
-  hexo.log.info(`ymd45921's Modified Volantis ${version}`);
+  const decorated = (() => {
+    const blank = 38 - 13 - version.length;
+    let align = '', str = ''
+    if (blank % 2 === 1) {
+      str = '自炊 Volantis ' + version;
+      align = String(' ').repeat(Math.floor(blank / 2)); 
+    } else {
+      str = '自炊 Volantis  ' + version;
+      align = String(' ').repeat(Math.floor(blank / 2) - 1); 
+    }
+    return align + str + align;
+  })();
+  hexo.log.info(`┌──────────────────────────────────────────┐`);
+  hexo.log.info(`│                                          │`);
+  hexo.log.info(`│  Using theme: ymd45921's Jisui Volantis  │`);
+  hexo.log.info(`│                                          │`);
+  hexo.log.info(`│  ${decorated}  │`);
+  hexo.log.info(`│        Based on Volantis 6.0-pjax        │`);
+  hexo.log.info(`│                                          │`);
+  hexo.log.info(`└──────────────────────────────────────────┘`);
 });
